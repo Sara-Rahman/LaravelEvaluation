2d27038734ec87d98fea16f4a4a91a2c07d5d699
Sara-Rahman <18103214@iubat.edu>
2022-10-31 01:37:09 +0600

    authetication done

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/home', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
